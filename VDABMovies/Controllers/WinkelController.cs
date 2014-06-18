c22ffefdb5da6adeef69f2eca17ce3a626a9891f
vdab-pierre @desktop <pierre.colin@vdab.be>
2014-06-18 08:34:18 +0200

    include some files

---
+++ VDABMovies/Controllers/WinkelController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VDABMovies.Models;
+
+namespace VDABMovies.Controllers
+{
+    public class WinkelController : Controller
+    {
+        // GET: Winkel
+        public ActionResult WinkelMandje()
+        {
+            Mandje mandje = null;
+            if (Session["mandje"] != null)
+            {
+                mandje = Session["mandje"] as Mandje;
+            }
+            MandjeViewModel vm = new MandjeViewModel();
+            vm.WinkelMandje = mandje;
+            return View(vm);
+        }
+
+        public ActionResult FilmVerwijderenUitMandje(int Id)
+        {
+            Mandje mandje = null;
+            if (Session["mandje"] != null)
+            {
+                mandje = Session["mandje"] as Mandje;
+                var reedsInMandje = mandje.Lijnen.Where(l => l.Film.Id == Id).FirstOrDefault();
+                if (reedsInMandje != null)
+                {
+
+                    mandje.Lijnen.Remove(reedsInMandje);
+                    Session["mandje"] = mandje;
+                    MandjeViewModel vm = new MandjeViewModel();
+                    vm.WinkelMandje = mandje;
+                    return View("WinkelMandje", vm);
+                }
+
+            }
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
---
+++ VDABMovies/Models/FilmBuddy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VDABMovies.Models
+{
+    public class FilmBuddy
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        [DisplayFormat(DataFormatString="{0:C}")]
+        public decimal Prijs { get; set; }
+        public int InVoorraad { get; set; }
+    }
+}
